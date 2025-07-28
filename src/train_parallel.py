309cb965784cc5e7990944916535003be0308b4b
Raphael <raphael.kropp@web.de>
2025-07-28 23:21:57 +0200

    bugfix and runs

--- src/train_parallel.py
+++ src/train_parallel.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
     print(cfg)
 
     processes = [
-        mp.Process(target=train_rnd, args=(cfg,)),
-        mp.Process(target=train_icm, args=(cfg,)),
-        #mp.Process(target=train_byol, args=(cfg,)),
-        #mp.Process(target=train_plain, args=(cfg,)),
+        #mp.Process(target=train_rnd, args=(cfg,)),
+        #mp.Process(target=train_icm, args=(cfg,)),
+        mp.Process(target=train_byol, args=(cfg,)),
+        mp.Process(target=train_plain, args=(cfg,)),
     ]
 
     for p in processes:
